8ecbdbbe34cc6b00ce2d8c940cef3e8a5630cedc
lezaanalberts <lezaanalbertscoding@gmail.com>
2024-03-26 12:50:38 +0200

    credits added

--- app/page.js
+++ app/page.js
@@ -33,6 +33,7 @@ export default function Home(){
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold mb-10 color:rgb(252 165 165)">Calculator</h1>
+      <h2 className="text-l font-bold">made by lezaanalberts on GitHub</h2>
       <div className=" bg-white p-6 rounded-lg shadow-lg">
         <input 
         type="text"
