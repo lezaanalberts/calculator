66c363bfbc9e1cff3ed2459d1cac8b8971df3658
lezaanalberts <lezaanalbertscoding@gmail.com>
2024-04-10 11:25:40 +0200

    change num to value

--- app/page.js
+++ app/page.js
@@ -32,9 +32,9 @@ export default function Home(){
 
   // ^2 function 
   function powerTwo({btn}){
-    return num * num;
+    return (value * value);
   }
-  
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold mb-10 color:rgb(252 165 165)">Calculator</h1>
