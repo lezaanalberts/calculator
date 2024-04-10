a61d9462af9f8282b4bcf93d36e1ffe7b95a5954
lezaanalberts <lezaanalbertscoding@gmail.com>
2024-04-10 11:21:36 +0200

    power two function

--- app/page.js
+++ app/page.js
@@ -30,6 +30,11 @@ export default function Home(){
     'C'
   ];
 
+  // ^2 function 
+  function powerTwo({btn}){
+    return num * num;
+  }
+  
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold mb-10 color:rgb(252 165 165)">Calculator</h1>
